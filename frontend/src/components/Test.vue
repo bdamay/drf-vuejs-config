53d7c09dd05bc9a834b2e0967c826b8f400c1a71
bdama <bdamay@gmail.com>
2020-10-13 15:07:09 +0200

    playing with Vue Component

--- frontend/src/components/Test.vue
+++ frontend/src/components/Test.vue
@@ -1,24 +1,36 @@
 <template>
   <div id="test-component">
-    <h2>Test component {{ name }}</h2>
-    <h3> {{ subname }}</h3>
+    <h2>Component Name: {{ name }}</h2>
+    <h3> Component subname: {{ subname }}</h3>
+    <h4>Play with input validation </h4>
+    <input type="text" class='input-lg' v-model="email"/>
+    <div class="alert" v-if="!email_valid">Email invalide réessayer</div>
   </div>
 </template>
 
 <script>
   export default {
     name: "Test",
-    data: function() {
+    data () {
       return {
         name: 'My component Name ',
-        subname: 'My component SubName'
+        subname: 'My component SubName',
+        email: ''
       }
+    },
+    computed: {
+      email_valid () {
+        return /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(this.email)
+      }
+
     }
+
   }
 </script>
 
 <style scoped>
-  h1 {
-    font-color: #cc5d24
+  .alert {
+    font-weight: bold;
+    color: #cc1d15
   }
 </style>
