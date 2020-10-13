e2404cb5a17803ff478ac226cd6a4e8bed16ab8a
bdama <bdamay@gmail.com>
2020-10-13 17:37:33 +0200

    Tests de QR code sur input

--- frontend/src/components/Test.vue
+++ frontend/src/components/Test.vue
@@ -4,13 +4,20 @@
     <h3> Component subname: {{ subname }}</h3>
     <h4>Play with input validation </h4>
     <input type="text" class='input-lg' v-model="email"/>
-    <div class="alert" v-if="!email_valid">Email invalide réessayer</div>
+    <div class="alert" v-if="!email_valid">La saisie n'est pas un email</div>
+
+
+    <H4>Test QRCODE component</H4>
+    <vue-qrcode :value="qrcode" :options="{ width: 200 }"></vue-qrcode>
   </div>
 </template>
 
 <script>
+
+import VueQrcode from '@chenfengyuan/vue-qrcode';
   export default {
     name: "Test",
+    components: {VueQrcode},
     data () {
       return {
         name: 'My component Name ',
@@ -21,6 +28,9 @@
     computed: {
       email_valid () {
         return /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(this.email)
+      },
+      qrcode () {
+        return this.email ? this.email : 'empty'
       }
 
     }
