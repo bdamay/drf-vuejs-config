180620cfe3d47736ff6c1157946abfdcdd2271b2
bdama <bdamay@gmail.com>
2020-12-09 17:12:58 +0100

    starter for integration vuecli

--- frontend/src/main.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-
-import App from './App.vue'
-
-new Vue({
-  el: '#app',
-  render: h => h(App)   //renders the App on Vue div (App est un module Vue
-})
-Vue.config.devtools = true;
